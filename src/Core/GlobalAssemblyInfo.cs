bafa488fe9f2a608e3f9931015ef3380779f37ec
Vladislav Borovikov <vborovikov@live.ru>
2016-04-04 22:48:16 +0500

    Bump version

--- src/Core/GlobalAssemblyInfo.cs
+++ src/Core/GlobalAssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Vladislav Borovikov")]
 [assembly: AssemblyProduct("MVVM and CQRS Toolkit for .NET Core/4.5 apps")]
-[assembly: AssemblyCopyright("Copyright © 2008-2015 Vladislav Borovikov")]
+[assembly: AssemblyCopyright("Copyright © 2008-2016 Vladislav Borovikov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: AssemblyInformationalVersion("2.1.2.0")]
