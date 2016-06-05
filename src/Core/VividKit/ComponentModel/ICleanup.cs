c55e6a3f72bbb23c9c1d55ba610c990355177abd
Vladislav Borovikov <vborovikov@live.ru>
2016-06-06 00:19:15 +0500

    Added comments

--- src/Core/VividKit/ComponentModel/ICleanup.cs
+++ src/Core/VividKit/ComponentModel/ICleanup.cs
@@ -1,7 +1,13 @@
 ﻿namespace Toolkit.ComponentModel
 {
+    /// <summary>
+    /// Defines a method to reset the state of an object.
+    /// </summary>
     public interface ICleanup
     {
+        /// <summary>
+        /// Resets the state.
+        /// </summary>
         void Cleanup();
     }
 }
